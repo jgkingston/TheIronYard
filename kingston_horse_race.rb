514705635f7819f265da09f43f39920c37266d98
Jamie Kingston <jamie.kingston@gmail.com>
2014-07-24 15:47:24 -0400

    Additional comments added

--- kingston_horse_race.rb
+++ kingston_horse_race.rb
@@ -14,7 +14,9 @@ each time the user hits enter the horses move a random distance. The user can us
 
 =end
 
+
 class Horse
+# Defines the horse class that will be used by the player and the computer
   @@list_of_horses = []
   attr_accessor :name
   attr_accessor :speed
@@ -31,6 +33,7 @@ class Horse
   end
 
   def self.add_horse *horse_attributes
+  # Creates a new horse object and return that object
     horse = Horse.new
     horse.name = horse_attributes[0]
     horse.speed = rand 3..5
@@ -41,10 +44,12 @@ class Horse
   end
 
   def self.display_horses
+  # Returns the array of horse objects
     @@list_of_horses
   end
 
   def run
+  # Uses the horse objects speed (with a dash modifier) to increment the horse's distance attribute
     dash = (rand 1..3) - 2
     self.distance += speed + dash
     puts "-"*distance + " " + self.name
@@ -53,12 +58,14 @@ class Horse
 end
 
 class Race
+# Define the Race class that stores the results of each race in a race object
   @@list_of_races = []
   attr_accessor :winner
   attr_accessor :time
   attr_accessor :cheat_count
 
   def self.add_race *race_attributes
+  # Creates a new race object with some stats from the race that was just run
     race = Race.new
     race.winner = race_attributes[0]
     race.time = race_attributes[1]
@@ -68,6 +75,7 @@ class Race
   end
 
   def self.display_races
+  # Outputs each race object in the array to its own line
     @@list_of_races.each do |race|
       p race
     end
@@ -76,6 +84,7 @@ class Race
 end
 
 def cheating racers, cheat
+# The cheating function takes cheat codes input by the user and then modifies horse objects based on the code string
   # puts cheat
   case cheat
   when "speed"
@@ -86,7 +95,7 @@ def cheating racers, cheat
     puts "Your horse has a jet pack."
   when "head start"
     racers[3].distance += 5
-    puts "Is this the starting line?"
+    puts "You somehow manage to jump ahead of the competition."
   when "sabotage"
     victim = rand 0..2
     racers[victim].speed = 1
@@ -101,12 +110,16 @@ def cheating racers, cheat
   # puts racers[3].name
 end
 
+# initializing variable to set the go condition
+# user can change this to false at the end of each race
 race_on = true
 
 while race_on
+# Main body of program. It generates horses and gets user input.
 
   horse_names = ["C Biscuit", "Carolina Chrome", "iHorse", "Ruby", "Urist McHorse", "Dog Biscuit", "Shadowfax"]
 
+  # Create an array of horse object that will determine in which track each horse runs
   racers = []
 
   horse_names.shuffle!
@@ -138,12 +151,14 @@ while race_on
   # p racers
   # p Horse.display_horses
 
+  # Create another array to keep track of the relative positino of each horse. Also determines winner at end.
   positions = []
 
   racers.each do |racer|
     positions[racers.index racer] = racer
   end
 
+  # Initializes the leader variable used in the while loop
   leader = positions[0]
 
   cheat_codes = ["speed", "ftl", "head start", "sabotage", "poison"]
@@ -158,6 +173,7 @@ while race_on
 
   track = " "*49 + "|"
 
+  # Puts starting display on the console
   puts "Announcer: 'And here we are at the starting line."
   puts track
   racers.each do |racer|
@@ -165,12 +181,15 @@ while race_on
   end
   puts track
 
+  # Initialize some more race stats that will increment with each loop
   counter = 0
 
   num_cheats = 0
 
+  # Main loop that makes the horses run and lets the player enter cheat codes
   while leader.distance < 50
 
+    # Player gets to use cheats codes
     puts "Alright you cheating bastard. What's it going to be? (Just press Enter for a clean race)"
 
     cheat = gets.chomp
@@ -179,20 +198,26 @@ while race_on
 
       cheating racers, cheat
 
+      # Increment num_cheat stat for race object
       num_cheats += 1
       
     end
 
+    # Racers run
     racers.each do |racer|
       racer.run
     end
 
+    # Display finish line "posts"
     puts " "*49 + "|"
 
+    # Determines the position of each horse on the track
     positions.sort! { |a,b| b.distance <=> a.distance }
 
+    # Assign leader based on position
     leader = positions[0]
 
+    # Increment time stat for race object
     counter += 1
 
     # p leader
@@ -203,10 +228,14 @@ while race_on
     
   end
 
+    # Display name of winner
     puts leader.name + " wins!"
     # p racers
+
+    # Create race object with stats from the race
     Race.add_race leader.name, counter, num_cheats
 
+    # Check to see if user would like to see their stats
     puts "Would you like to see the results of your race(s)? (y/n)"
 
     display = gets.chomp
@@ -215,6 +244,7 @@ while race_on
       Race.display_races
     end
 
+    # Check to see if the user wants to play again
     puts "Would you like to run another race? (y/n)"
 
     again = gets.chomp
